f52d882d94d682b754c7d60e9238e866d75a918a
takami.sato <takami.sato@brainpad.co.jp>
2014-09-28 18:31:06 +0900

    kmeans法をscikit-leanに変更

--- sample/sample_extract_surf/sample_extract_feature.py
+++ sample/sample_extract_surf/sample_extract_feature.py
@@ -5,7 +5,9 @@ import numpy
 import glob
 from os import path
 import re
-from scipy.cluster.vq import vq, kmeans, kmeans2, whiten
+from sklearn.cluster import MiniBatchKMeans
+from logging import getLogger
+logger = getLogger(__name__)
 
 IMG_ID_REGREX = re.compile(r'(\d+)\.(jpg)|(JPG)|(jpeg)|(JPEG)$')
 KMEANS_ITER_NUM = 100
@@ -27,12 +29,15 @@ class SampleExtractFeature(object):
         self.cluster_num = cluster_num
 
         self.map_surf_features = self._extract_surf_features()
-        self.centroid, self.label = self._calc_kmeans_centroid()
+        self.kmeans = self._calc_kmeans_centroid()
         self.map_bag_of_keypoints = self._calc_bag_of_keypoints()
 
     def _extract_surf_features(self):
-
+        logger.debug("enter")
         images = glob.glob(self.folder_path + '*.jpg')
+
+        if len(images) == 0:
+            Exception("%s has no image file."%sself.folder_path)
         map_surf_features = {}
         for img_path in images:
             image_id = IMG_ID_REGREX.search(img_path).group(1)
@@ -43,41 +48,53 @@ class SampleExtractFeature(object):
                                                        cv.CreateMemStorage(),
                                                        (1 , 500, 3, 4))
             map_surf_features[image_id] = descriptors
+        logger.debug("exit")
 
         return map_surf_features
 
     def _calc_kmeans_centroid(self):
+        logger.debug("enter")
 
         image_data = []
         for surf_feature in self.map_surf_features.values():
             image_data += surf_feature
         image_data = numpy.array(image_data)
         #print image_data
+        """
         centroid, label = kmeans2(image_data,
                                   k=self.cluster_num,
                                   minit="points",
                                   iter=KMEANS_ITER_NUM)
-        return centroid, label
+        """
+        kmeans = MiniBatchKMeans(n_clusters=self.cluster_num,
+                                 max_iter=KMEANS_ITER_NUM)
+
+        label = kmeans.fit_predict(image_data)
+        centroid = kmeans.cluster_centers_
+        logger.debug("exit")
+
+        return kmeans
 
     def _calc_bag_of_keypoints(self):
+        logger.debug("enter")
 
         map_bag_of_keypoints = {}
         for img_id, descriptors in self.map_surf_features.items():
 
             bag_of_keypoints = numpy.zeros(self.cluster_num)
-            for descriptor in descriptors:
-                centroid_distances = [numpy.linalg.norm(self.centroid[i] - descriptor)
-                                      for i in xrange(self.cluster_num)]
 
-                nearest_centroid = numpy.argsort(centroid_distances)[0]
+            for nearest_centroid in self.kmeans.predict(descriptors):
                 bag_of_keypoints[nearest_centroid] += 1
 
             bag_of_keypoints /= len(descriptors)
             map_bag_of_keypoints[img_id] = bag_of_keypoints
 
+        logger.debug("exit")
+
         return map_bag_of_keypoints
 
     def to_csv(self, csv_path):
+        logger.debug("enter")
 
         with open(csv_path, "w") as f:
             f.write("img_id,")
@@ -89,4 +106,5 @@ class SampleExtractFeature(object):
                 f.write(",".join(map(str, bag_of_keypoints)))
                 f.write("\n")
 
+        logger.debug("exit")
 
