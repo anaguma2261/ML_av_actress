47a473056e61dd77118b8e6a4576e4716bc34961
tsuda7 <tsuda.h.aa@gmail.com>
2014-10-25 15:54:38 +0900

    新規画像を受け取れるようにするための、sample_extract_feature.py のリファクタリング

--- sample/sample_extract_surf/sample_extract_feature.py
+++ sample/sample_extract_surf/sample_extract_feature.py
@@ -1,5 +1,5 @@
-#! /usr/bin/python
 # -*- encoding: utf-8 -*-
+
 import cv
 import numpy
 import glob
@@ -7,10 +7,6 @@ from os import path
 import re
 from sklearn.cluster import MiniBatchKMeans
 from logging import getLogger
-logger = getLogger(__name__)
-
-IMG_ID_REGREX = re.compile(r'(\d+)\.(jpg)|(JPG)|(jpeg)|(JPEG)$')
-KMEANS_ITER_NUM = 100
 
 class SampleExtractFeature(object):
     '''
@@ -19,28 +15,31 @@ class SampleExtractFeature(object):
     :param cluster_num int: 画像特徴の次元数
     '''
 
+    IMG_ID_REGREX = re.compile(r'(\d+)\.(jpg)|(JPG)|(jpeg)|(JPEG)$')
+    KMEANS_ITER_NUM = 100
+    logger = getLogger(__name__)
 
-    def __init__(self,
-                 folder_path,
-                 cluster_num=100):
-
+    def __init__(self, folder_path, cluster_num=100):
 
         self.folder_path = folder_path
         self.cluster_num = cluster_num
 
-        self.map_surf_features = self._extract_surf_features()
-        self.kmeans = self._calc_kmeans_centroid()
-        self.map_bag_of_keypoints = self._calc_bag_of_keypoints()
+        map_surf_features = self._extract_surf_features()
+        self.kmeans       = self._calc_kmeans_centroid(map_surf_features)
+        self.bags_of_keypoints = { img_id: self._calc_a_bag_of_keypoints(descriptors)
+                                            for img_id, descriptors
+                                             in map_surf_features.items() }
 
     def _extract_surf_features(self):
-        logger.debug("enter")
+        self.logger.debug("enter")
         images = glob.glob(self.folder_path + '*.jpg')
 
         if len(images) == 0:
-            Exception("%s has no image file."%sself.folder_path)
+            raise Exception("%s has no image file."%sself.folder_path)
+
         map_surf_features = {}
         for img_path in images:
-            image_id = IMG_ID_REGREX.search(img_path).group(1)
+            image_id = self.IMG_ID_REGREX.search(img_path).group(1)
             img = cv.LoadImageM(img_path , cv.CV_LOAD_IMAGE_GRAYSCALE)
 
             #: ハードコーディングすみません 意味をその内調べます。
@@ -48,15 +47,15 @@ class SampleExtractFeature(object):
                                                        cv.CreateMemStorage(),
                                                        (1 , 500, 3, 4))
             map_surf_features[image_id] = descriptors
-        logger.debug("exit")
+        self.logger.debug("exit")
 
         return map_surf_features
 
-    def _calc_kmeans_centroid(self):
-        logger.debug("enter")
+    def _calc_kmeans_centroid(self, map_surf_features):
+        self.logger.debug("enter")
 
         image_data = []
-        for surf_feature in self.map_surf_features.values():
+        for surf_feature in map_surf_features.values():
             image_data += surf_feature
         image_data = numpy.array(image_data)
         #print image_data
@@ -67,44 +66,34 @@ class SampleExtractFeature(object):
                                   iter=KMEANS_ITER_NUM)
         """
         kmeans = MiniBatchKMeans(n_clusters=self.cluster_num,
-                                 max_iter=KMEANS_ITER_NUM)
+                                 max_iter=self.KMEANS_ITER_NUM)
 
         label = kmeans.fit_predict(image_data)
         centroid = kmeans.cluster_centers_
-        logger.debug("exit")
+        self.logger.debug("exit")
 
         return kmeans
 
-    def _calc_bag_of_keypoints(self):
-        logger.debug("enter")
+    def _calc_a_bag_of_keypoints(self, descriptors):
+        bag_of_keypoints = numpy.zeros(self.cluster_num)
 
-        map_bag_of_keypoints = {}
-        for img_id, descriptors in self.map_surf_features.items():
+        for nearest_centroid in self.kmeans.predict(descriptors):
+            bag_of_keypoints[nearest_centroid] += 1
 
-            bag_of_keypoints = numpy.zeros(self.cluster_num)
-
-            for nearest_centroid in self.kmeans.predict(descriptors):
-                bag_of_keypoints[nearest_centroid] += 1
-
-            bag_of_keypoints /= len(descriptors)
-            map_bag_of_keypoints[img_id] = bag_of_keypoints
-
-        logger.debug("exit")
-
-        return map_bag_of_keypoints
+        bag_of_keypoints /= len(descriptors)
+        return bag_of_keypoints
 
     def to_csv(self, csv_path):
-        logger.debug("enter")
+        self.logger.debug("enter")
 
         with open(csv_path, "w") as f:
             f.write("img_id,")
             f.write(",".join(["feature_%s"%i for i in xrange(self.cluster_num)]))
             f.write("\n")
 
-            for img_id, bag_of_keypoints in self.map_bag_of_keypoints.items():
+            for img_id, bag_of_keypoints in self.bags_of_keypoints.items():
                 f.write(str(img_id)+",")
                 f.write(",".join(map(str, bag_of_keypoints)))
                 f.write("\n")
 
-        logger.debug("exit")
-
+        self.logger.debug("exit")
--- sample/sample_extract_surf/test_avactre.py
+++ sample/sample_extract_surf/test_avactre.py
@@ -19,7 +19,7 @@ class TestSampleExtractFeature(unittest.TestCase):
 
         img_ids = ["11", "13", "17", "23", "25", "27", "3", "32", "33", "4", "43",
                     "46", "6", "60", "61", "63", "79", "8", "80", "88", "89"]
-        for img_id in  sef.map_surf_features.keys():
+        for img_id in  sef._extract_surf_features().keys():
             self.assertTrue(img_id in img_ids)
 
         sef.to_csv("test.csv")
